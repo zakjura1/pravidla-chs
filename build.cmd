b8f2a26cefbc19eb590124d9da8dc52419819c47
Lumír Návrat <lumi@centrum.cz>
2020-12-23 10:43:00 +0100

    SL2021: opet zmena struktury. zjednoduseni aby sly odkazy napric dokumenty

--- build.cmd
+++ build.cmd
@@ -1,17 +1,13 @@
-call asciidoctor-pdf -v -a lang=cs pravidla/pravidla_SL.adoc -D target
+call asciidoctor-pdf -v -a lang=cs src/asciidoc/pravidla_sl.adoc -D target
+call asciidoctor -v -a lang=cs src/asciidoc//pravidla_sl.adoc -D target
 
-call asciidoctor -v -a lang=cs pravidla/pravidla_SL.adoc -D target
-
-call asciidoctor-pdf -v -a lang=cs provadeci_predpisy_dospeli\Provadeci_predpisy_dospeli_2020.adoc -D target
-
-call asciidoctor -v -a lang=cs provadeci_predpisy_dospeli\Provadeci_predpisy_dospeli_2020.adoc -D target
+call asciidoctor-pdf -v -a lang=cs src/asciidoc/provadeci_predpisy_dospeli.adoc -D target
+call asciidoctor -v -a lang=cs src/asciidoc/provadeci_predpisy_dospeli.adoc -D target
 
 
-call asciidoctor-pdf -v -a lang=cs provadeci_predpisy_mladez\Provadeci_predpisy_dospeli_2020.adoc -D target
-
-call asciidoctor -v -a lang=cs provadeci_predpisy_mladez\Provadeci_predpisy_mladez_2020.adoc -D target
+call asciidoctor-pdf -v -a lang=cs src/asciidoc/provadeci_predpisy_dospeli.adoc -D target
+call asciidoctor -v -a lang=cs src/asciidoc/provadeci_predpisy_mladez.adoc -D target
 
 
 call asciidoctor-pdf -v -a lang=cs vyjimky_z_pravidel_U14\Vyjimky_z_pravidel_pro_U14_2020.adoc -D target
-
-call asciidoctor -v -a lang=cs vyjimky_z_pravidel_U14\Vyjimky_z_pravidel_pro_U14_2020.adoc -D target
+call asciidoctor -v -a lang=cs src/asciidoc/vyjimky_z_pravidel_pro_u14.adoc -D target
--- convert.cmd
+++ convert.cmd
@@ -2,5 +2,5 @@ pandoc --from=docx --to=asciidoc --wrap=none --markdown-headings=atx --extract-m
 pandoc --from=docx --to=asciidoc --wrap=none --markdown-headings=atx --extract-media=extracted-media Provadeci_predpisy_mladez_2020.docx > Provadeci_predpisy_mladez_2020.adoc
 pandoc --from=docx --to=asciidoc --wrap=none --markdown-headings=atx --extract-media=extracted-media Vyjimky_z_pravidel_pro_U14_2020.docx > Vyjimky_z_pravidel_pro_U14_2020.adoc
   
-pandoc --from docbook --to html --output pravidla_SL-pandoc.html pravidla_SL.xml
-pandoc --from docbook --to latex --output pravidla_SL-pandoc.pdf pravidla_SL.xml
+pandoc --from docbook --to html --output pravidla_sl-pandoc.html pravidla_sl.xml
+pandoc --from docbook --to latex --output pravidla_sl-pandoc.pdf pravidla_sl.xml
